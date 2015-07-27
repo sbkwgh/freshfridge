ecefe6504d90f5ba0ca50f040a3c07b59c7fa2ca
sbkwgh <sbkwgh@gmail.com>
2015-07-27 19:10:58 +0100

    Add user accoutnts feature

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var config = require('config.js');
+
+var routes;
+var db;
+
+//Problem here
+mongoose.connect('mongodb://localhost/fridgething')
+db = mongoose.connection;
+
+app.set('views', './templates');
+app.set('view engine', 'twig');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+})); 
+app.use(cookieParser(config.cookieSecret));
+
+
+routes = require('./router.js')(app);
+app.use(express.static('./public'))
+
+db.once('open', function(){
+	console.log('Connected to db');
+	app.listen(3000, function() {
+		console.log('Listening on port 3000');
+	});
+})
---
+++ routes/index.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user.js');
+
+//Index page and login screen
+router.get('/', function(req, res) {
+	if(req.signedCookies.loggedIn === 'true') {
+		res.redirect('/home');
+	} else {
+		res.render('index')
+	}
+});
+
+//Logged in homepage
+router.get('/home', function(req, res) {
+	if(req.signedCookies.loggedIn !== 'true') {
+		res.redirect('/')
+	} else {
+		res.render('home');
+	}
+})
+
+//Route to which the form sends
+router.post('/login', function(req, res) {
+	User.checkAccount(
+		req.body.username,
+		req.body.password,
+		function(correct) {
+			if(correct) {
+				res.cookie('loggedIn', 'true', {signed: true});
+				res.redirect('/home')
+			} else {
+				res.render('login', {
+					errors: [
+						'Wrong username or password'
+					]
+				});
+			}
+		}
+	)
+})
+
+//Route to which the form sends
+router.post('/createAccount', function(req, res) {
+	var newUser = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
+	newUser.save(function(err) {
+		if(err) {
+			if(err.code === 11000) {
+				res.render('login', {
+					errors: [
+						'Username already exists'
+					]
+				})
+			} else {
+				res.render('login', {
+					errors: [
+						'It\'s us, not you. Something went wrong on our end, so try again later.'
+					]
+				})
+			}
+		} else {
+			res.cookie('loggedIn', true, {signed: true});
+			res.redirect('/home')
+		}
+	});
+})
+
+router.get('/logout', function(req, res) {
+	res.cookie('loggedIn', false, {signed: true});
+	res.redirect('/');
+})
+
+module.exports = router;
