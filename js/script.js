b2ae53cea9a06493d98c8f67e661c6d5431808a7
sbkwgh <sebiddy@gmail.com>
2015-08-24 08:14:54 +0000

    Update 'store' to use websql and localstorage

--- js/script.js
+++ js/script.js
@@ -36,37 +36,142 @@ function $(selectorOrParentSelector, returnOneOrChildSelector, returnOne) {
 	return null;
 }
 
+var webSQL = {
+	db: null,
+	onError: function(tx, err) {
+		console.log(err);
+	},
+	onSuccess: function() {
+		console.log('success');
+	},
+	openDb: function() {
+		var dbSize = 5 * 1024 * 1024;
+		
+		if(!openDatabase) {
+			store.onError();
+			return;
+		}
+	
+		this.db = openDatabase('store', 1, 'database for the app', dbSize);
+	},
+	createTable: function() {
+		var db = this.db;
+		
+		db.transaction(function(tx) {
+			tx.executeSql('CREATE TABLE IF NOT EXISTS items(ID INTEGER PRIMARY KEY ASC, name TEXT, imageURL TEXT, expiryDate DATETIME)', []);
+		});
+	},
+	add: function(obj) {
+		var db = this.db;
+		
+		db.transaction(function(tx) {
+			tx.executeSql(
+				'INSERT INTO items(name, imageURL, expiryDate) VALUES(?, ?, ?)',
+				[obj.name, obj.imageURL, obj.expiryDate],
+				this.onSuccess, 
+				this.onError
+			);
+		});
+	},
+	get: function(cb) {
+		var db = this.db;
+		
+		function success(tx, results) {
+			var items = [];
+			
+			for(var i = 0; i < results.rows.length; i++) {
+				items.push(results.rows.item(i));
+			}
+			
+			cb(items);
+		}
+		
+		db.transaction(function(tx) {
+			tx.executeSql(
+				'SELECT * FROM items',
+				[],
+				success,
+				this.onError
+			);
+		});
+	},
+	remove: function(index) {
+		var db = this.db;
+		
+		db.transaction(function(tx) {
+			tx.executeSql(
+				'DELETE FROM items WHERE ID=?',
+				[index],
+				this.onSuccess,
+				this.onError
+			);
+		});
+	},
+	update: function(index, item) {
+		var db = this.db;
+		
+		db.transaction(function(tx) {
+			tx.executeSql(
+				'UPDATE items SET name=?, imageURL=?, expiryDate=? WHERE ID=?',
+				[item.name, item.imageURL, item.expiryDate, index],
+				this.onSuccess,
+				this.onError
+			);
+		});
+	}
+};
 var store = {
-	get: function(name) {
+	init: function() {
+      
+		this.webSQL.openDb();
+		this.webSQL.createTable();
+	},
+	get: function(name, cb) {
 		console.log('get ' + name);
 
-		var items = JSON.parse(localStorage.getItem(name));
-
-		return items || [];
+		if(name === 'items') {
+			this.webSQL.get(cb);
+		} else {
+			var items = JSON.parse(localStorage.getItem(name));
+			return items || [];
+		}
 
 	},
- 	post: function(name, item) {
- 		console.log('post ' + name);
+	add: function(name, item) {
+		console.log('post ' + name);
 
- 		var items = this.get(name);
- 		items.push(item);
+		if(name === 'items') {
+			this.webSQL.add(item);
+		} else {
+			var items = this.get(name);
+			items.push(item);
 
- 		localStorage.setItem(name, JSON.stringify(items));
- 	},
- 	update: function(name, index, updatedItem) {
- 		console.log('update ' + name);
+			localStorage.setItem(name, JSON.stringify(items));
+		}
+	},
+	update: function(name, index, updatedItem) {
+		console.log('update ' + name);
 
- 		var items =  this.get(name);
- 		items[index] = updatedItem;
+		if(name === 'items') {
+			this.webSQL.update(index, updatedItem);
+		} else {
+			var items =  this.get(name);
+			items[index] = updatedItem;
 
- 		localStorage.setItem(name, JSON.stringify(items))
- 	},
- 	'delete': function(name, index) {
- 		console.log('delete ' + name);
+			localStorage.setItem(name, JSON.stringify(items));
+		}
+	},
+	remove: function(name, index) {
+		console.log('delete ' + name);
 
- 		var items = this.get(name);
- 		items.splice(index, 1);
+		if(name === 'items') {
+			this.webSQL.remove(index);
+		} else {
+			var items = this.get(name);
+			items.splice(index, 1);
 
- 		localStorage.setItem(name, JSON.stringify(items));
- 	}
- };
+			localStorage.setItem(name, JSON.stringify(items));
+		}
+	},
+	webSQL: webSQL
+};
