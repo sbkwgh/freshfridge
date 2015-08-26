749cb273e85065e745a18ded4c877f9617d1ed91
sbkwgh <sebiddy@gmail.com>
2015-08-26 09:42:25 +0000

    Add ability to star recipes

--- js/recipeView.js
+++ js/recipeView.js
@@ -1,11 +1,9 @@
 function scroll(el, sign) {
-	var sign = -sign;
+	sign = -sign;
 	var width = -el.offsetWidth * 0.925 * $('.recipe-card').length - $('.recipe-card', 1).offsetWidth * 0.025;
 
 	var currentLeft = +el.style.left.slice(0, -2);
 	var newScrollLeft = currentLeft + Math.round(el.offsetWidth * sign * 0.82);
-
-	console.log(newScrollLeft + ' ' + width * 0.59538688785)
 
 	if(newScrollLeft <= width * 0.4) {
 		newScrollLeft = width + 'px';
@@ -23,22 +21,25 @@ mainHammer.on('swipeleft', function(ev) {
 	if(ev.target.classList.contains('recipe-card')) {
 		scroll(ev.target.parentElement, +1);
 	}
-}) 
+});
 mainHammer.on('swiperight', function(ev) {
 	if(ev.target.classList.contains('recipe-card')) {
 		scroll(ev.target.parentElement, -1);
 	}
-})
+});
 
 var recipeView = {
 	template: $('#recipeTemplate', 1).innerHTML,
 	created: function() {
-		this.$dispatch('menuItems', [])
+		this.$dispatch('menuItems', []);
+		
 	},
 	data: function() {
 		return {
 			ingredients: '',
+			starred: false,
 			hideRecipe: true,
+			starredRecipes: store.get('starredRecipes'),
 			recipeCategories: [
 				{
 					title: 'Breakfast',
@@ -108,10 +109,21 @@ var recipeView = {
 		};
 	},
 	methods: {
-		toggleRecipeCard: function(recipeIndex, ev) {
-			var categoryIndex = +ev.target.parentElement.previousElementSibling.id;
-			this.ingredients = this.recipeCategories[categoryIndex].recipeCards[recipeIndex].ingredients;
-
+		toggleRecipeCard: function(recipeIndex, categoryIndex, ev) {
+			var recipe = this.recipeCategories[categoryIndex].recipeCards[recipeIndex];
+			
+			this.starred = false;
+			
+			for(var i = 0; i < this.starredRecipes.length; i++) {
+				if(this.starredRecipes[i].image === recipe.image) {
+					this.starred = true
+				}
+			}
+			
+			this.ingredients = recipe.ingredients;
+			this.categoryIndex = categoryIndex;
+			this.recipeIndex = recipeIndex;
+			
 			ev.target.classList.toggle('recipe-card-full-width');
 			ev.target.parentElement.nextElementSibling.classList.toggle('recipe-hidden');
 		},
@@ -122,6 +134,41 @@ var recipeView = {
 		},
 		recipeUrl: function() {
 
+		},
+		starRecipeCard: function() {
+			var recipe = this.recipeCategories[this.categoryIndex].recipeCards[this.recipeIndex];
+			var recipeStarred = false;
+			var starRecipeCardIndex = null;
+			
+			for(var i = 0; i < this.starredRecipes.length; i++) {
+				if(recipe.image === this.starredRecipes[i].image) {
+					recipeStarred = true;
+					starRecipeCardIndex = i;
+				}
+			}
+			
+			if(!recipeStarred) {
+				this.starredRecipes.push(recipe);
+				store.add('starredRecipes', recipe);
+			} else {
+				this.starredRecipes.$remove(starRecipeCardIndex);
+				store.remove('starredRecipes', this.recipeIndex);
+			}
+			this.starred = !this.starred;
+		},
+		toggleStarredRecipeCard: function(recipeIndex, ev) {
+			this.ingredients = this.starredRecipes[recipeIndex].ingredients;
+			this.recipeIndex = recipeIndex;
+			this.starred = true;
+			ev.target.classList.toggle('recipe-card-full-width');
+			ev.target.parentElement.nextElementSibling.classList.toggle('recipe-hidden');
+		},
+		unstarStarredRecipeCard: function(ev) {
+			var recipe = ev.target.parentElement.parentElement.parentElement;
+			$('.recipe-card-full-width', 1).classList.toggle('recipe-card-full-width');
+			recipe.classList.toggle('recipe-hidden');
+			this.starredRecipes.$remove(this.recipeIndex)
+			store.remove('starredRecipes', this.recipeIndex);
 		}
 	}
 };
