7cb397d1d90e6032307d8541d511d27114d06cec
sbkwgh <sbkwgh@gmail.com>
2015-08-30 09:05:52 +0100

    Remove non-api parts of fridgething project

--- router.js
+++ router.js
@@ -1,13 +1,7 @@
-var index = require('./routes/index.js');
-var account = require('./routes/account.js');
 var api = require('./routes/api.js');
-var item = require('./routes/api/item.js');
 var recipes = require('./routes/api/recipes.js');
 
 module.exports = function(app) {
-	app.use('/', index);
-	app.use('/account', account);
 	app.use('/api', api);
-	app.use('/api/item', item);
 	app.use('/api/recipes', recipes);
 };
